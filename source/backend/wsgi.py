e10af2d83fd22a37c6cd6b7e3cc1fc445889837b
Myhailo Chernyshov <mykhailo.chernyshov@raccoongang.com>
2023-02-07 01:04:28 +0200

    Configuration is updated

--- source/backend/wsgi.py
+++ source/backend/wsgi.py
@@ -12,7 +12,6 @@ import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 os.environ.setdefault('DJANGO_CONFIGURATION', 'Prod')
 
-from configurations.wsgi import get_wsgi_application  # pylint: disable=wrong-import-position
-
+from configurations.wsgi import get_wsgi_application  # pylint: disable=wrong-import-position  # noqa: E402
 
 application = get_wsgi_application()
