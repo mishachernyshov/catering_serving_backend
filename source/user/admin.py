878f20db7c31d342c2ce7b432e01e8aca91f4b27
Myhailo Chernyshov <mykhailo.chernyshov@raccoongang.com>
2023-03-09 01:17:40 +0200

    User application models are added

--- source/user/admin.py
+++ source/user/admin.py
@@ -1,3 +1,37 @@
 """
 User administration module.
 """
+from django.contrib import admin
+from django.contrib.admin.sites import NotRegistered
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group
+
+from user.models import UserProfile
+
+User = get_user_model()
+
+
+class UserProfileInline(admin.StackedInline):
+    """
+    Inline admin interface for UserProfile model.
+    """
+
+    model = UserProfile
+    can_delete = False
+
+
+class UserAdmin(BaseUserAdmin):
+    """
+    Admin interface for the User model.
+    """
+
+    inlines = (UserProfileInline,)
+
+
+try:
+    admin.site.unregister(User)
+    admin.site.unregister(Group)
+except NotRegistered:
+    pass
+admin.site.register(User, UserAdmin)
--- source/user/models.py
+++ source/user/models.py
@@ -1,3 +1,33 @@
 """
 User module models.
 """
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class UserProfile(models.Model):
+    """
+    Additional data about a user.
+    """
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birthday = models.DateField()
+    balance = models.FloatField()
+
+    def __str__(self):
+        return self.user.username  # pylint: disable=no-member
+
+
+class UserBalanceChange(models.Model):
+    """
+    User balance changing values.
+    """
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    amount = models.FloatField()
+    transaction_datetime = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.user.username}: {self.amount} ({self.transaction_datetime})'  # pylint: disable=no-member
